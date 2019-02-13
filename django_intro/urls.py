6b68e9ff2569a5e79cf7fa4d41e7df7977f3bd99
yooseungju <seung338989@gmail.com>
2019-02-13 01:49:44 +0000

    02_wellcome django

--- django_intro/urls.py
+++ django_intro/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views
 
 urlpatterns = [
+    path('home/index/', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from pprint import pprint
 
 # Create your views here.
+
+def index(request):
+    # print(request)
+    # print(type(request))
+    # pprint(request.META)
+    return HttpResponse('Welcome to Django !')
+    
